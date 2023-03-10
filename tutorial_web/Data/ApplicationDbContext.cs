d6ea1a162c4449a31184ccdcecdde9afd5eed250
LimTangmeng <limtangmeng100@gmail.com>
2023-03-10 16:41:39 +0700

    First Commit

---
+++ tutorial_web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using tutorial_web.Models;
+
+namespace tutorial_web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<tutorial_web.Models.Jokes> Jokes { get; set; }
+    }
+}
---
+++ tutorial_web/Models/Jokes.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tutorial_web.Models
+{
+    public class Jokes
+    {
+        [Required(ErrorMessage = "ID is required")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "JokeQuestion is required")]
+        public string JokeQuestion { get; set; }
+
+        [Required(ErrorMessage = "JokeAnswer is required")]
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
